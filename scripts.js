e1dbb7be59b5e7377dab7841e8526d137fd4aa58
Tyra-hans <tyrahans17@gmail.com>
2019-08-15 22:46:12 +0300

    add js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+var cc= YY[2][3] + 1;
+
+
+
+function calculate (){
+  var dayOfTheWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7;
+if (dayOfTheWeek > 7 || dayOfTheWeek <1){
+  window.alert("Please enter valid birthday");
+  return ;
+}
+
+
+}
+var dayOfTheWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+
+var day= [Sunday , Monday , Tuesday , Wednesday , Thursday , Friday , Saturday];
+var
