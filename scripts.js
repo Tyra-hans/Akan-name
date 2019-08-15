699b031665dd7601d38a92b7a13bb9362920a344
Tyra-hans <tyrahans17@gmail.com>
2019-08-16 00:32:54 +0300

    add first correction and indentation changes

--- scripts.js
+++ scripts.js
@@ -1,27 +1,13 @@
-//var cc= YY[2][3] + 1;
-
 
 function getValue()
 {
   var male = document.getElementById("male");
   var female= document.getElementById("female");
-    if (male.checked==true)
-    alert("you checked: " + male.value);
-   else if(female.checked==true)
+  if (male.checked==true)
+  alert("you checked: " + male.value);
+  else if(female.checked==true)
   alert("you checked: " +female.value);
 
-else
-alert("please select a gender");
+  else
+  alert("please select a gender");
 }
-
-//function calculate (){
-  //var dayOfTheWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7;
-//if (dayOfTheWeek > 7 || dayOfTheWeek <1){
-//  window.alert("Please enter valid birthday");
-//  return ;
-//
-
-//var dayOfTheWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
-
-//var
